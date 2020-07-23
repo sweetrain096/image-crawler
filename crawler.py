c4f3c4f0953330691fc68dc4f0192a49e33b5b61
sweetrain096 <gpfhddl09@gmail.com>
2020-07-23 17:22:27 +0900

    add get_search_query for call function from js

--- crawler.py
+++ crawler.py
@@ -5,30 +5,27 @@ from selenium import webdriver
 import urllib, urllib.request
 import time
 
+# search_word = "식빵"
+# total_cnt = 100
+# Dir = "D:/rain/image-crawler/img/"
 
-
-search_word = "식빵"
-total_cnt = 100
-url = "https://www.google.com/search?q="
-url_source = "&source=lnms&tbm=isch"
-
-Dir = "D:/rain/image-crawler/img/"
-
-def search_image(word):
+def get_search_query(search_word, total_cnt, Dir):
+    search_image(search_word, total_cnt, Dir)
+  
+def search_image(search_word, total_cnt, Dir):
+    url = "https://www.google.com/search?q="
+    url_source = "&source=lnms&tbm=isch"
     # selenium으로 크롬 드라이버 열기
     driver = webdriver.Chrome('./chromedriver')
     # 모든 자원이 로드될 때까지 기다리는 시간을 임의 지정(안해도 됨)
     driver.implicitly_wait(1)
-    driver.get(url+word+url_source)
+    driver.get(url+search_word+url_source)
 
 
     first_html = driver.page_source
     first_soup = BeautifulSoup(first_html, 'html.parser')
-    # pprint.pprint(soup)
     imgs = first_soup.select("img.Q4LuWd")
-    # print(imgs)
     img_cnt = len(imgs)
-    # print(img_cnt)
 
     SCROLL_PAUSE_TIME = 0.5
 
@@ -62,8 +59,8 @@ def search_image(word):
         if image.get('data-src'):
             image_list.append(image.get('data-src'))
 
-    print(len(image_list), image_list)
-    save(image_list[:total_cnt])
+    # print(len(image_list), image_list)
+    save(image_list[:total_cnt], Dir, search_word)
 
 
 def create_folder(save_dir):
@@ -73,7 +70,7 @@ def create_folder(save_dir):
     except OSError:
         print("Error : Creating directory.")
 
-def save(arr):
+def save(arr, Dir, search_word):
     save_dir = Dir+search_word
     create_folder(save_dir)
 
@@ -81,4 +78,4 @@ def save(arr):
         urllib.request.urlretrieve(src, save_dir + "/" + str(i) + ".jpg")
         # print(i, "saved")
 
-search_image(search_word)
+# search_image(search_word, total_cnt, Dir)
